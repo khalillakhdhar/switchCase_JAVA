82d2948fe8410018dc4ea8907d67707677abb94e
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 15:51:55 +0100

    Update Days.java

--- src/cases/Days.java
+++ src/cases/Days.java
@@ -2,6 +2,18 @@ package cases;
 
 public class Days {
 private int day;
+private int year;
+
+
+
+
+public int getYear() {
+	return year;
+}
+
+public void setYear(int year) {
+	this.year = year;
+}
 
 public int getDay() {
 	return day;
@@ -14,5 +26,31 @@ public void setDay(int day) {
 public Days(int day) {
 	this.day = day;
 }
+
+
+String dayName()
+{
+	switch (this.day) {
+	case 1: 
+		return "lundi";
+	case 2:
+		return "mardi";
+	case 3:
+		return "mercredi";
+	case 4:
+		return "jeudi";
+	case 5:
+		return "vendredi";
+	case 6:
+		return "samedi";
+	case 7: 
+		return "dimanche";
+	default:
+		return "jour incorrecte";
+	}	
+
+
+}
+
 
 }
