1ad585fc25dd963f5ad78ec1da9c956fa3347c0c
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 15:46:51 +0100

    Update Days.java

--- src/cases/Days.java
+++ src/cases/Days.java
@@ -3,4 +3,16 @@ package cases;
 public class Days {
 private int day;
 
+public int getDay() {
+	return day;
+}
+
+public void setDay(int day) {
+	this.day = day;
+}
+
+public Days(int day) {
+	this.day = day;
+}
+
 }
