9ddd9d728fc245c5cfe0da9c5c0e29447506f392
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 15:34:53 +0100

    Initial commit

---
+++ src/switchCase/Test.java
@@ -0,0 +1,26 @@
+package switchCase;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print(intervale());
+	}
+static int intervale()
+{
+int month=7;
+switch(month)
+{
+case 1,3,5,7,8,10,12:
+	return 31;
+case 2:
+	return 28;
+case 4,6,9,11:
+	return 30;
+default: 
+	return 30;
+
+
+}
+}
+}
