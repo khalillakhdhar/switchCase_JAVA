193f189474348c0d87c14991f9924daf6607919f
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-05 15:41:05 +0100

    division

--- src/ifElse/Division.java
+++ src/ifElse/Division.java
@@ -1,5 +1,36 @@
 package ifElse;
 
 public class Division {
+private int a;
+
+public int getA() {
+	return a;
+}
+
+public void setA(int a) {
+	this.a = a;
+}
+public String parite()
+{
+if(this.a%2==0)
+	return "paire";
+else
+	return "impaire";
+}
+public String divisible()
+{
+int c=this.a/100;
+int d=(this.a%100)/10;
+int u=this.a%10;
+int somme=c+d+u;
+if(somme%3==0)
+	return "divisible par 3";
+else
+	return "non divisible par 3";
+
+
+
+
+}
 
 }
